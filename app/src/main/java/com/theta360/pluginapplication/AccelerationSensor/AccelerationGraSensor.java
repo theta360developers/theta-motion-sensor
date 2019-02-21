3dd98486e62722fbd9af077863bb8969db8006bc
Craig <craig@oppkey.com>
2019-02-21 15:08:54 -0800

    first commit

---
+++ app/src/main/java/com/theta360/pluginapplication/AccelerationSensor/AccelerationGraSensor.java
@@ -0,0 +1,57 @@
+package com.theta360.pluginapplication.AccelerationSensor;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import java.util.List;
+
+public class AccelerationGraSensor implements SensorEventListener {
+
+    private static final float FILTER = 0.1f; // フィルタ係数
+
+    private float[] currentGravityValues      = {0.0f, 0.0f, 0.0f};
+    private float[] currentAccelerationValues = {0.0f, 0.0f, 0.0f};
+
+    public synchronized float getX() {
+        return this.currentAccelerationValues[0];
+    }
+
+    public synchronized float getY() {
+        return this.currentAccelerationValues[1];
+    }
+
+    public synchronized float getZ() {
+        return this.currentAccelerationValues[2];
+    }
+
+    public AccelerationGraSensor(SensorManager sensorManager) {
+        // イベントリスナーの登録
+        Sensor s = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, s, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            // ローパスフィルタで重力値を抽出
+            currentGravityValues[0] = event.values[0] * FILTER + currentGravityValues[0] * (1.0f - FILTER);
+            currentGravityValues[1] = event.values[1] * FILTER + currentGravityValues[1] * (1.0f - FILTER);
+            currentGravityValues[2] = event.values[2] * FILTER + currentGravityValues[2] * (1.0f - FILTER);
+
+            synchronized (this) {
+                // 重力値の除去
+                currentAccelerationValues[0] = event.values[0] - currentGravityValues[0];
+                currentAccelerationValues[1] = event.values[1] - currentGravityValues[1];
+                currentAccelerationValues[2] = event.values[2] - currentGravityValues[2];
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
---
+++ app/src/main/java/com/theta360/pluginapplication/MainActivity.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright 2018 Ricoh Company, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.theta360.pluginapplication;
+
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.theta360.pluginapplication.AccelerationSensor.AccelerationGraSensor;
+import com.theta360.pluginapplication.task.TakePictureTask;
+import com.theta360.pluginapplication.task.TakePictureTask.Callback;
+import com.theta360.pluginlibrary.activity.PluginActivity;
+import com.theta360.pluginlibrary.callback.KeyCallback;
+import com.theta360.pluginlibrary.receiver.KeyReceiver;
+import com.theta360.pluginlibrary.values.LedColor;
+import com.theta360.pluginlibrary.values.LedTarget;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends PluginActivity {
+    private TakePictureTask.Callback mTakePictureTaskCallback = new Callback() {
+        @Override
+        public void onTakePicture(String fileUrl) {
+
+        }
+    };
+
+
+    // specific to sensor tutorial
+    private SensorManager graSensorManager;
+    private AccelerationGraSensor accelerationGraSensor;
+
+    private static final int ACCELERATION_INTERVAL_PERIOD = 1000;
+    private Timer timer;
+    private static final float ACCELERATION_THRESHOLD = 3.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //加速度を取れる状態に設定
+        graSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerationGraSensor = new AccelerationGraSensor(graSensorManager);
+
+        // Set enable to close by pluginlibrary, If you set false, please call close() after finishing your end processing.
+        setAutoClose(true);
+        // Set a callback when a button operation event is acquired.
+        setKeyCallback(new KeyCallback() {
+            @Override
+            public void onKeyDown(int keyCode, KeyEvent event) {
+                if (keyCode == KeyReceiver.KEYCODE_CAMERA) {
+                    /*
+                     * To take a static picture, use the takePicture method.
+                     * You can receive a fileUrl of the static picture in the callback.
+                     */
+                    new TakePictureTask(mTakePictureTaskCallback).execute();
+                }
+
+            }
+
+            @Override
+            public void onKeyUp(int keyCode, KeyEvent event) {
+                /**
+                 * You can control the LED of the camera.
+                 * It is possible to change the way of lighting, the cycle of blinking, the color of light emission.
+                 * Light emitting color can be changed only LED3.
+                 */
+                notificationLedBlink(LedTarget.LED3, LedColor.BLUE, 1000);
+            }
+
+            @Override
+            public void onKeyLongPress(int keyCode, KeyEvent event) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // 加速度のログを出力
+                Log.d("accelerX", String.valueOf(accelerationGraSensor.getX()));
+                Log.d("accelerY", String.valueOf(accelerationGraSensor.getY()));
+                Log.d("accelerZ", String.valueOf(accelerationGraSensor.getZ()));
+
+                if (Math.abs(accelerationGraSensor.getX()) > ACCELERATION_THRESHOLD ||
+                        Math.abs(accelerationGraSensor.getY()) > ACCELERATION_THRESHOLD ||
+                        Math.abs(accelerationGraSensor.getZ()) > ACCELERATION_THRESHOLD) {
+                    new TakePictureTask(mTakePictureTaskCallback).execute();
+                }
+//
+            }
+        }, 0, ACCELERATION_INTERVAL_PERIOD);
+    }
+
+    @Override
+    protected void onPause() {
+        // Do end processing
+        //close();
+
+        super.onPause();
+        timer.cancel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (graSensorManager != null) {
+            // イベントリスナーの解除
+            graSensorManager.unregisterListener(accelerationGraSensor);
+        }
+    }
+}
